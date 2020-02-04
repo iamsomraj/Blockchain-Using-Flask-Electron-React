ede1d370d6adc98531afe645d403db8143175d35
iamsomraj <iamsomraj@gmail.com>
2020-02-05 04:11:17 +0530

    Updates

---
+++ precache-manifest.88a2f74d2a69315e7fbf330c27e65cb8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77b52d410a4e2ad8df9ae92d4d26ee6a",
+    "url": "/Blockchain-Using-Flask-Electron-React/index.html"
+  },
+  {
+    "revision": "f348a5b302d4594e19c4",
+    "url": "/Blockchain-Using-Flask-Electron-React/static/js/2.e4302c79.chunk.js"
+  },
+  {
+    "revision": "967c3aa677d48917bda81b3b2ff2693c",
+    "url": "/Blockchain-Using-Flask-Electron-React/static/js/2.e4302c79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7e56963b78dc18bafcb",
+    "url": "/Blockchain-Using-Flask-Electron-React/static/js/main.56a3444c.chunk.js"
+  },
+  {
+    "revision": "5a8a4adb65dbc1fef7bb",
+    "url": "/Blockchain-Using-Flask-Electron-React/static/js/runtime-main.29577aaa.js"
+  }
+]);
